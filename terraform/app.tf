dccf580b466deb7e9f960eda4c21303734747f11
ravichhatani <57475701+ravichhatani@users.noreply.github.com>
2021-07-30 15:04:10 +0800

    Update app.tf

--- terraform/app.tf
+++ terraform/app.tf
@@ -9,7 +9,7 @@ variable "team-name" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-04542dadb1b2718d3"
+  ami           = "ami-013f17f36f8b1fefb"
   instance_type = "t2.micro"
 
   tags = {
